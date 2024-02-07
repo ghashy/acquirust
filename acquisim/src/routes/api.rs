68ebe16c0fea6e2a3f9c090073b6adabe658742f
ghashy <obsidian.musicwork@gmail.com>
2024-02-07 22:58:06 +0300

    implement InitPaymentRequest type

--- acquisim/src/routes/api.rs
+++ acquisim/src/routes/api.rs
@@ -1,25 +1,51 @@
+use std::collections::BTreeMap;
+
+use anyhow::Context;
 use axum::{
     extract::State, http::StatusCode, response::IntoResponse, routing, Json,
     Router,
 };
-use secrecy::Secret;
+use secrecy::{ExposeSecret, Secret};
 use serde::{Deserialize, Serialize};
+use sha2::{self, Digest, Sha256};
 use tokio::sync::TryLockError;
+use url::Url;
 
 use crate::{bank::BankOperationError, error_chain_fmt, startup::AppState};
 
 // ───── Request Types ────────────────────────────────────────────────────── //
 
-#[derive(Deserialize)]
+/// Initial payment operation, basic of acquiring
+#[derive(Serialize, Deserialize)]
 pub struct InitPaymentRequest {
-    initiator_card: uuid::Uuid,
-    initiator_password: Secret<String>,
+    notification_url: Url,
+    success_url: Url,
+    fail_url: Url,
     amount: i64,
+    token: String,
+}
+
+impl InitPaymentRequest {
+    pub fn generate_token(&self, cashbox_password: &Secret<String>) -> String {
+        let mut token_map = BTreeMap::new();
+        token_map.insert("notification_url", self.notification_url.to_string());
+        token_map.insert("success_url", self.success_url.to_string());
+        token_map.insert("fail_url", self.fail_url.to_string());
+        token_map.insert("amount", self.amount.to_string());
+        token_map.insert("password", cashbox_password.expose_secret().clone());
+
+        let concatenated: String = token_map.into_values().collect();
+        let mut hasher: Sha256 = Digest::new();
+        hasher.update(concatenated);
+        let hash_result = hasher.finalize();
+
+        // Convert hash result to a hex string
+        format!("{:x}", hash_result)
+    }
 }
 
 #[derive(Serialize)]
 pub struct InitPaymentResponse {
-    #[serde(rename = "PaymentURL")]
     payment_url: url::Url,
 }
 
@@ -27,10 +53,14 @@ pub struct InitPaymentResponse {
 
 #[derive(thiserror::Error)]
 enum ApiError {
+    #[error("Unexpected error")]
+    UnexpectedError(#[from] anyhow::Error),
     #[error("Mutex lock error: {0}")]
     MutexLockError(#[from] TryLockError),
     #[error("Bank operation error: {0}")]
     BankOperationError(#[from] BankOperationError),
+    #[error("Unauthorized operation")]
+    UnauthorizedError,
 }
 
 impl std::fmt::Debug for ApiError {
@@ -49,6 +79,12 @@ impl IntoResponse for ApiError {
             ApiError::BankOperationError(_) => {
                 StatusCode::BAD_REQUEST.into_response()
             }
+            ApiError::UnauthorizedError => {
+                StatusCode::UNAUTHORIZED.into_response()
+            }
+            ApiError::UnexpectedError(_) => {
+                StatusCode::INTERNAL_SERVER_ERROR.into_response()
+            }
         }
     }
 }
@@ -64,6 +100,15 @@ pub fn api_router(state: AppState) -> Router {
 async fn init_payment(
     State(state): State<AppState>,
     Json(req): Json<InitPaymentRequest>,
-) -> Result<StatusCode, ApiError> {
-    Ok(StatusCode::OK)
+) -> Result<Json<InitPaymentResponse>, ApiError> {
+    // Authorize request
+    let token = req.generate_token(&state.settings.terminal_settings.password);
+
+    if !token.eq(&req.token) {
+        return Err(ApiError::UnauthorizedError);
+    }
+
+    Ok(Json(InitPaymentResponse {
+        payment_url: "".parse().context("Failed to parse url")?,
+    }))
 }
