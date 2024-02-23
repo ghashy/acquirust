caceb310b568de51a6dabe1cb848c8d4aceb8aee
ghashy <obsidian.musicwork@gmail.com>
2024-02-23 09:14:09 +0300

    add InitPayment stub

--- backends/acquisim-api/src/lib.rs
+++ backends/acquisim-api/src/lib.rs
@@ -1,14 +1,23 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use acquiconnect::{ApiAction, Client, ClientError, Url};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+struct InitPayment;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+struct Request;
+struct Response;
+
+impl ApiAction for InitPayment {
+    type Request = Request;
+    type Response = Response;
+
+    fn url_path(&self) -> &'static str {
+        "Init"
+    }
+
+    async fn perform_action(
+        req: Self::Request,
+        addr: Url,
+        client: &Client,
+    ) -> Result<Self::Response, ClientError> {
+        todo!()
     }
 }
